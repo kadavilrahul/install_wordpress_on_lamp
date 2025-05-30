6aa0e39a647f596d84cc226e477cecbb8ec8936a
Rahul <kadavil.rahul@gmail.com>
2025-05-30 17:55:58 +0530

    Update script

--- examples/ssl_renewal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Add cron job for SSL renewal
-(crontab -l 2>/dev/null; echo "0 0,12 * * * python3 -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew --quiet") | crontab -
-
-echo "SSL renewal cron job added."
